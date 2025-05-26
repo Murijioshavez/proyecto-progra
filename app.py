caf1ec03636b6d93d9d18b552ab7f82a39ac2570
Murijioshavez <cecam.m.chavez7a@gmail.com>
2025-05-26 17:59:20 -0600

    Ver actividades funcionando
    necesita desc?

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template
+from registro_csv import leer_csv, guardar_en_csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", nombre= 'Mau')
+    return render_template("index.html")
 @app.route('/nueva')
 def nueva():
     return render_template("nueva.html")
 
 @app.route('/actividades')
 def actividades():
-    return render_template("actividades.html")
+    actividades = leer_csv('/actividades.csv')
+    return render_template("actividades.html", actividades=actividades)
+
 
 if __name__ == "__main__":
     app.run(debug=True)

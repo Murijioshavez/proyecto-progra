27a4ea22373e8c296aea82caa74cdf841c8189e7
Murijioshavez <cecam.m.chavez7a@gmail.com>
2025-05-26 17:13:40 -0600

    Navbar funcional

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html", nombre= 'Mau')
-@app.route('/login')
-def login():
-    return render_template("login.html")
+@app.route('/nueva')
+def nueva():
+    return render_template("nueva.html")
+
+@app.route('/actividades')
+def actividades():
+    return render_template("actividades.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

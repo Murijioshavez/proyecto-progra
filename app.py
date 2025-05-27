23c58ccbd218dd2e420ba884f241b44c5f771145
Murijioshavez <cecam.m.chavez7a@gmail.com>
2025-05-26 21:25:30 -0600

    ya todo jalando
    uwu

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from registro_csv import leer_csv, guardar_en_csv
 
 app = Flask(__name__)
@@ -13,8 +13,20 @@ def nueva():
 @app.route('/actividades')
 def actividades():
     actividades = leer_csv('/actividades.csv')
-    return render_template("actividades.html", actividades=actividades)
+    return render_template("actividades.html", actividades=actividades[::-1])
 
+@app.route("/publicar", methods=["GET", "POST"])
+def publicar():
+    if request.method == "POST":
+        categoria = request.form["categoria"]
+        descripcion = request.form["descripcion"]
+        duracion = request.form["duracion"]
+        comentario = request.form["comentario"]
 
+        guardar_en_csv('actividades.csv',{'categoria': categoria,'descripcion': descripcion,'duracion': duracion,'comentario': comentario}, ['categoria','descripcion','duracion','comentario'] )
+
+        return redirect("/actividades")  
+
+    return render_template("actividades.html")  
 if __name__ == "__main__":
     app.run(debug=True)

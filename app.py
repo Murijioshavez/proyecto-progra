688b8da836e2a37c9beb7df60f30d6ad125c5f8d
Murijioshavez <cecam.m.chavez7a@gmail.com>
2025-06-21 21:01:32 -0600

    redeployment

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def publicar():
 
     return render_template("actividades.html")  
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Usa 5000 si no se define otro puerto
+    port = int(os.environ.get("PORT", 10000))  # Usa 5000 si no se define otro puerto
     app.run(host="0.0.0.0", port=port)
